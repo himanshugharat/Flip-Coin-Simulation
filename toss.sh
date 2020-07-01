78ca06e1fcc0948ec684bdc6c0e7fdf601f34e5a
himanshugharat <himanshugharat44@gmail.com>
2020-07-01 12:42:12 +0530

    add the tie property

--- toss.sh
+++ toss.sh
@@ -1,23 +1,33 @@
 #!/bin/bash -x
 hwin=0
 twin=0
+res(){
+	toss=$((RANDOM%2))
+       	if [ $toss -eq 1 ]
+        then
+                echo "head"
+                ((hwin++))
+        else
+                echo "tails"
+                ((twin++))
+        fi
+
+}
 while [ $hwin -lt 21 ] && [ $twin -lt 21 ]
 do
-	toss=$((RANDOM%2))
-	if [ $toss -eq 1 ]
-	then
-		echo "head"
-		((hwin++))
-	else
-		echo "tails"
-		((twin++))
-	fi
+	res
 done
 echo "no of time head won"$hwin
 echo "no of times tail won"$twin
-if [ $hwin -gt $twin ]
+if [ $hwin -eq $twin ]
 then
-	echo "head won by"$((hwin-twin))
+	res
 else
-	echo "tails won by"$((hwin-twin))
+
+	if [ $hwin -gt $twin ]
+	then
+		echo "head won by"$((hwin-twin))
+	else
+		echo "tails won by"$((hwin-twin))
+	fi
 fi
