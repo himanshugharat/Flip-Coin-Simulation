88f563a84d252205d193d53909c3012ee5a54fd8
himanshugharat <himanshugharat44@gmail.com>
2020-07-01 12:32:22 +0530

    continue till either wons 21 time

--- toss.sh
+++ toss.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 hwin=0
 twin=0
-for((i=0;i<10;i++))
+while [ $hwin -lt 21 ] && [ $twin -lt 21 ]
 do
 	toss=$((RANDOM%2))
 	if [ $toss -eq 1 ]
@@ -15,3 +15,9 @@ do
 done
 echo "no of time head won"$hwin
 echo "no of times tail won"$twin
+if [ $hwin -gt $twin ]
+then
+	echo "head won by"$((hwin-twin))
+else
+	echo "tails won by"$((hwin-twin))
+fi
